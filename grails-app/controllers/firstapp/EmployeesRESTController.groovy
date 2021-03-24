e41b5b0636624f7a96d7b1eef148c442d55013f4
sphantom99 <georgeal1999@gmail.com>
2021-03-24 12:39:55 +0200

    Fixed #15

--- grails-app/controllers/firstapp/EmployeesRESTController.groovy
+++ grails-app/controllers/firstapp/EmployeesRESTController.groovy
@@ -2,8 +2,20 @@ package firstapp
 
 class EmployeesRESTController {
 
+    def employeeService
     def addEmployee() {
-
-
+        def firstName = params.first_name
+        def lastName = params.last_name
+        def afm = params.afm
+        def year = params.dob_year
+        def month = params.dob_month
+        def day = params.dob_day
+        def department = params.department
+        def dob = year + "-" + month + "-" + day
+       // def args = [firstName, lastName, afm, dob, department]
+        def ret = employeeService.addEmployee(firstName, lastName, afm, dob, department)
+        if (ret!='error'){
+            respond status: 200
+        }
     }
 }
